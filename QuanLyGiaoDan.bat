4dcb2fd6a8378f1bd0ff1cc9cc2c35f0c30fbb76
SutrixNgocHoang <ngoc.hm@sutrixsolutions.com>
2025-10-18 10:39:42 +0700

    REvert bat and icon file

---
+++ QuanLyGiaoDan.bat
@@ -0,0 +1,54 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCuDJGiR9UwcMTxdTQumPWWGN6Ad5sTiyOOGrWkcWss8d4GV07eBQA==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJGiR9UwcMTxdTQumPWWGN6Ad5sTiyOOGrWkcWqw6YIq7
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+REM Chuyển đến thư mục public
+cd /d %~dp0\public
+
+REM Chạy server PHP tại localhost:8080 bằng php\php.exe trong dự án
+start "PHP Server" cmd /c "..\php\php.exe -S localhost:8080"
+
+REM Đợi server khởi động một chút
+ping 127.0.0.1 -n 5 > nul
+
+REM Mở trình duyệt Chrome nếu có, nếu không thì mở trình duyệt mặc định
+set "URL=http://localhost:8080"
+
+REM Thử mở Chrome
+start "" "chrome" "%URL%"
+if errorlevel 1 (
+    REM Nếu Chrome không có, mở trình duyệt mặc định
+    start "" "%URL%"
+)
+
+pause
